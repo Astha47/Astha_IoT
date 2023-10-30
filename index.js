7df0ea06ae6540d52620dbd3bb906ee2fb6eeb11
Anas Fathurrahman <anasfathurrahman.edu@gmail.com>
2023-10-30 08:50:33 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/action', (req, res) => {
+  res.send('ON14');
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
