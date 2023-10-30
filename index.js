0100a2c333ec44d1ce994cf83720953e3e982d0a
Anas Fathurrahman <anasfathurrahman.edu@gmail.com>
2023-10-30 08:52:56 +0700

    create main get

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Astha IoT Server')
+})
+
 app.get('/action', (req, res) => {
   res.send('ON14');
 });
