4a408808f93d408c7fe0f3e96e5dfd6b2b493950
Anas Fathurrahman <anasfathurrahman.edu@gmail.com>
2023-11-04 10:02:36 +0700

    add counter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let count = 0;
+
 app.get('/', (req, res) => {
     res.send('Astha IoT Server')
 })
@@ -10,6 +12,8 @@ app.get('/action', (req, res) => {
   const actions = ['ON16', 'ON14', 'ON27'];
   const randomAction = actions[Math.floor(Math.random() * actions.length)];
   res.send(randomAction);
+  count +=1;
+  console.log("Server dipanggil : ", count)
 });
 
 app.get('/waduh', (req, res) => {
